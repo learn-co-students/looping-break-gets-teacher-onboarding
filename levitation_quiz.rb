135c0086278babd1c70fd21bb05dfe99e22dc11b
Levi Miller <paullevimiller@gmail.com>
2019-07-15 12:40:59 -0700

    Done.

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -1,6 +1,12 @@
 
 def levitation_quiz
 	#your code here
+	puts "What is the spell that enacts levitation?"
+	spell = ""
+	while spell != "Wingardium Leviosa"
+		spell = gets
+	end
+	puts "You passed the quiz!"
 end
 
 
